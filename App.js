9b89ed519d25afc92179ccb0fcba834f62761f35
Atirath-PC\Atirath <atikosireddy98@gmail.com>
2020-06-26 15:37:41 -0700

    top bar finished
    Got all the widgets in the top bar finished

--- App.js
+++ App.js
@@ -4,19 +4,28 @@ import css from './style.css';
 export default function App() {
 	return (
 		<View style={styles.container}>
-			<div className='top-bar'>
-				<h1>Parker Miller</h1>
-				<div className='nav-tab'>
-					<h2>Followers</h2>
-				</div>
-				<div className='nav-tab'>
-					<h2>Following</h2>
+		<div className='top-bar-padding'>
+				<div className='top-bar'>
+					<span className='back-arrow'></span>
+					<div className='center-header-container'>
+						<h1>Parker Miller</h1>
+					</div>
+					<span className='ellipses'>
+						<div className='ellipse'></div>
+						<div className='ellipse'></div>
+						<div className='ellipse'></div>
+					</span>
+					<span className='nav-tab'>
+						<h2>Followers</h2>
+					</span>
+					<span className='nav-tab' style={{float: 'right'}}>
+						<h2>Following</h2>
+					</span>
 				</div>
 			</div>
 			<div className='following-section'>
 				<div className='following-box'></div>
 				<div className='following-box'></div>
-
 			</div>
 		</View>
 	);
